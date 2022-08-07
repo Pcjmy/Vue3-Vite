0b088d148e135f608b1687da3a408dee39d365cf
Pcjmy <1716045191@qq.com>
2022-08-07 22:21:42 +0800

    submit-1

---
+++ vue/src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory("/"),
+  routes: routes,
+})
+
+/**
+ * 输出对象
+ */
+export default router;
---
+++ vue/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    主页
+    <br/>
+    <el-button type="primary">
+      Hello
+      <el-icon><Edit /></el-icon>
+
+      <Edit style="width: 1em; height: 1em; margin-right: 8px" />
+      <Position style="width: 1em; height: 1em; margin-right: 8px" />
+    </el-button>
+  </div>
+</template>
+
+<script setup>
+import {Edit, Position} from '@element-plus/icons-vue'
+</script>
