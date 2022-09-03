e6aab686b01a557993d25e0200680de50050993f
Pcjmy <1716045191@qq.com>
2022-09-03 11:10:47 +0800

    前台CRUD

--- vue/README.md
+++ vue/README.md
@@ -16,3 +16,20 @@
 ### 组件
 
 [Table 表格](https://element-plus.gitee.io/zh-CN/component/table.html)
+
+[Button 按钮](https://element-plus.gitee.io/zh-CN/component/button.html)
+
+[Icon 图标](https://element-plus.gitee.io/zh-CN/component/icon.html)
+
+[Dialog 对话框](https://element-plus.gitee.io/zh-CN/component/dialog.html)
+
+### dialog表单
+
+- 完成前端 dialog表单数据的校验
+- 完成前端新增数据接口的调用
+- 完成数据的刷新
+-  state.form = JSON.parse(JSON.stringify(row)) 实现了数据的转让
+- 新增的时候注意初始化数据
+- 保存的时候如何区分编辑还是新增？
+- request.delete()  这个你改了吗？ 接口的请求方式要注意
+- 使用`el-popconfirm` 来保护删除按钮
--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,19 +1,116 @@
 <template>
-  <el-table :data="state.tableData" stripe style="width: 360px">
-    <el-table-column prop="id" align="center" label="ID" width="180"/>
-    <el-table-column prop="username" align="center" label="用户名" />
+  <div>
+    <el-button type="primary" @click="handleAdd"><el-icon style="margin-right: 3px"><Plus /></el-icon>新增</el-button>
+  </div>
+  <el-table :data="state.tableData" stripe style="width: 100%">
+    <el-table-column prop="id" label="ID" width="180" />
+    <el-table-column prop="username" label="用户名" />
+    <el-table-column label="操作">
+      <template #default="scope">
+        <el-button type="primary" size="small" @click="handleEdit(scope.row)"><el-icon style="margin-right: 3px"><Edit /></el-icon> 编辑</el-button>
+        <el-popconfirm title="确定删除吗?" @confirm="deleteRow(scope.row.id)"  confirm-button-text="确定" cancel-button-text="取消">
+          <template #reference>
+            <el-button link type="danger" size="small"><el-icon style="margin-right: 3px"><Delete /></el-icon> 删除</el-button>
+          </template>
+        </el-popconfirm>
+      </template>
+    </el-table-column>
   </el-table>
+
+  <el-dialog v-model="dialogFormVisible" title="用户信息" width="40%">
+    <el-form :model="state.form" :rules="state.rules" ref="ruleFormRef" label-width="120px" style="width: 85%">
+      <el-form-item label="用户名" prop="username">
+        <el-input v-model="state.form.username" autocomplete="off" placeholder="请输入用户名" />
+      </el-form-item>
+    </el-form>
+    <template #footer>
+      <span class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取消</el-button>
+        <el-button type="primary" @click="save">保存</el-button>
+      </span>
+    </template>
+  </el-dialog>
 </template>
 
 <script setup>
-import {Edit, Position} from '@element-plus/icons-vue'
-import {reactive} from "vue";
+import {Edit, Delete, Plus} from '@element-plus/icons-vue'
+import {getCurrentInstance,reactive,ref} from "vue";
 import request from "../request";
+import {ElMessage} from "element-plus"
+const {proxy} = getCurrentInstance()
+
+// getCurrentInstance用于获取当前组件的实例
+// proxy是响应式的
+// console.log(proxy);
 
 const state = reactive({
-  tableDate: []
+  tableDate: [],
+  form: {},
+  rules: {
+    username: [
+      { required: true, message: '请输入用户名', trigger: 'blur' },
+      { min: 3, message: '最少输入3位', trigger: 'blur' }
+    ]
+  }
 })
-request.get("/user").then(res => {
-  state.tableData = res
-})
+
+const load = () => {
+  request.get("/user").then(res => {
+    state.tableData = res
+  })
+}
+load()
+
+const dialogFormVisible = ref(false)
+const handleAdd = () => {
+  dialogFormVisible.value = true
+  state.form = {}  // 初始化数据
+}
+
+const handleEdit = (row) => {
+  dialogFormVisible.value = true
+  state.form = JSON.parse(JSON.stringify(row))
+}
+
+const save = () => {
+  proxy.$refs.ruleFormRef.validate((valid) => {
+    if (valid) {
+      state.form.password = '123456'
+
+      console.log(state.form)
+      if (state.form.id) { // 如果id存在，那就是编辑
+        // 请求后台接口
+        request.put("/user", state.form).then(res => {
+          ElMessage.success("保存成功")
+          // 关闭弹窗
+          dialogFormVisible.value = false
+          // 刷新表格
+          load()
+        })
+      } else {
+        // 请求后台接口
+        request.post("/user", state.form).then(res => {
+          ElMessage.success("保存成功")
+          // 关闭弹窗
+          dialogFormVisible.value = false
+          // 刷新表格
+          load()
+        })
+      }
+
+    }
+  })
+}
+
+const deleteRow = (id) => {
+  request.delete("/user/" + id).then(res => {  //  /user/1
+    if (res === true) {
+      ElMessage.success("删除成功")
+    } else {
+      ElMessage.success("删除失败")
+    }
+    // 刷新表格
+    load()
+  })
+}
 </script>
